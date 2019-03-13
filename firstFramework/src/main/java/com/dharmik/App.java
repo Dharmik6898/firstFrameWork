a3b519d6995ee672de366632acb3d3e99f53fa82
Dharmik Patel <dharmikpatel0608@gmail.com>
2019-03-12 21:40:46 -0300

    Initial Commit

---
+++ firstFramework/src/main/java/com/dharmik/App.java
@@ -0,0 +1,13 @@
+package com.dharmik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
